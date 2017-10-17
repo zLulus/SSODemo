6e5cf5e47430ac75b242bf5038b037c604697ef9
Lulus <865514198@qq.com>
2017-10-17 09:41:42 +0800

    ......

---
+++ IdentityServerApi/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServerApi.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Redis()
+        {
+            return View();
+        }
+    }
+}
